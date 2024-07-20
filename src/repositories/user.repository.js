b18aac518b96245871577a168d7649c5c087684c
Majid Khorshidian <mkhorshidian72@gmail.com>
2024-07-20 21:58:58 +0330

    Add user repository

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,5 +1,37 @@
 const { UserModel } = require("../models");
 
-class UserRepository {}
+const createUser = async (userData) => {
+  const user = new UserModel(userData);
+  return user.save();
+};
 
-module.exports = UserRepository;
+const findUserById = async (id) => {
+  return UserModel.findById(id);
+};
+
+const findUserByEmail = async (email) => {
+  return UserModel.findOne({ email });
+};
+
+const updateUserById = async (id, updateData) => {
+  return UserModel.findByIdAndUpdate(id, updateData, {
+    new: true,
+  });
+};
+
+const deleteUserById = async (id) => {
+  return UserModel.findByIdAndDelete(id);
+};
+
+const comparePassword = async (user, password) => {
+  return UserModel.isPasswordMatch(password);
+};
+
+module.exports = {
+  createUser,
+  findUserById,
+  findUserByEmail,
+  updateUserById,
+  deleteUserById,
+  comparePassword,
+};
